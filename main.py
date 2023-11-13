4e4bb467c5bb3d14d4e4db47613b9f1c3f8e1f2d
lili7642 <lili7642@student.uu.se>
2023-11-13 13:13:32 +0100

    read data from parent parent folder

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import os
+
+
 print("Hello World!")
 
+current_directory = os.path.dirname(__file__)
+f_path = current_directory+"/../../project_course_data/pilot3.txt"
+
+if os.path.exists(f_path):
+    with open(f_path, "r") as f:
+        f_content = f.read()
+        print(f_content)
+
+
+ 
