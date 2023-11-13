e9496b8bae6ccb1147f5f9c744d78dd9b7abf635
lili7642 <lili7642@student.uu.se>
2023-11-13 14:08:15 +0100

    read data to df

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import os
+import pandas as pd
 
 
 print("Hello World!")
 
+## ================= READING DATA ====================================
+
 current_directory = os.path.dirname(__file__)
 f_path = current_directory+"/../../project_course_data/pilot3.txt"
 
-if os.path.exists(f_path):
-    with open(f_path, "r") as f:
-        f_content = f.read()
-        print(f_content)
+# import dataset as panda dataframe, skip last rows with text
+df = pd.read_fwf(f_path, nrows=100426)
+# add option for viewing all column
+pd.set_option('display.max_columns', None)
+# remove column with "->"
+df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
+# Split IP address and port to two columns
+df[['Src IP Addr', 'Src Port']] = df['Src IP Addr:Port'].str.split(':', n=1, expand=True)
+df[['Dst IP Addr', 'Dst Port']] = df['Dst IP Addr:Port'].str.split(':', n=1, expand=True)
+"Drop old combined column"
+df = df.drop(columns=["Src IP Addr:Port", "Dst IP Addr:Port","Flows"])
+"Rename columns"
+df = df.rename(columns={"seen": "Time", "Date first": "Date"})
+print(df.head(415))
 
+## ====================================================================
  
