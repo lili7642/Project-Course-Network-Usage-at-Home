f52428eba1c2c6ac654ea71894c6f1e26cd33c18
Jesper Edstrom <jesper.j.edstrom@gmail.com>
2023-11-28 16:18:53 +0100

    added ml_model code for nn classifyer

--- ml_model.py
+++ ml_model.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+import numpy as np
+from sklearn.neural_network import MLPClassifier
 
 pd.set_option('display.max_columns', None)
 
@@ -11,9 +12,56 @@ f_path = current_directory + "/../../project_course_data/"
 csv_filename = "owndata_processed.csv"
 
 # read csv
-data = pd.read_csv(f_path + csv_filename, sep=",", na_values="NaN")
+data = pd.read_csv(f_path + csv_filename, sep="\t", na_values="NaN")
+data = data.drop(data[data.Label == "Unknown"].index)
+data = data.drop(data[data.Duration > 20000].index)
 
-# 
+
+# separate data into train and test data
+data = data.drop(columns=["Date", "Time", "Src_IP_Addr", "Dst_IP_Addr", "Host_IP", "Rate", "Domain_Name", "Adblocker"])
+
+msk = np.random.rand(len(data)) < 0.8
+train = data[msk]
+test = data[~msk]
+
+Xtrain = train.drop(columns=["Label"])
+Xtest = test.drop(columns=["Label"])
+
+Ytrain = train["Label"]
+Ytest = test["Label"]
+
+# print(Xtest)
+
+
+# print(train)
+
+# print(np.size(train))
+# print(np.size(test))
+
+# # nn model using lbfgs 
+clf = MLPClassifier(solver='sgd', alpha=1e-5, hidden_layer_sizes=(10, 2), random_state=1)
+
+model = clf.fit(Xtrain, Ytrain)
+
+Yhat = model.predict(Xtest)
+
+# print(Yhat)
+# print(Ytest)
+def accuracy(yhat,ytest):
+    print(yhat)
+    print(ytest)
+    count = 0
+    for ind, _ in enumerate(yhat):
+        if yhat[ind] == ytest[ind]:
+            count += 1
+    
+    print(count/len(ytest))
+
+
+accuracy(Yhat,np.array(Ytest))
+
+
+
 
 
 
--- split_categories.py
+++ split_categories.py
@@ -15,21 +15,22 @@ data = pd.read_csv(f_path + csv_filename, sep=",", na_values="NaN")
 # data2 = pd.read_csv(f_path + csv_hostnames, sep="\t", na_values="NaN", index_col= 0)
 
 # data = data.drop(data["192.168.8" in data["Src_IP_Addr"]].index)
-data = data[data["Src_IP_Addr"].str.contains("192.168.8") == False] 
+data2 = data[data["Src_IP_Addr"].str.contains("192.168.8") == False] 
 # data = data.drop(data[data["Src_IP_Addr"] == "192.168.8.196"].index)
 
  
 # get the dummies and store it in a variable
-dummies = pd.get_dummies(data.Proto)
-dummies2 = pd.get_dummies(data.Src_Port)
+dummies = pd.get_dummies(data.Proto).astype(int)
+dummies2 = pd.get_dummies(data2.Src_Port).astype(int)
  
 # Concatenate the dummies to original dataframe
 data = pd.concat([data, dummies], axis='columns')
 data = pd.concat([data, dummies2], axis='columns')
 
 # drop the values
-data.drop(['Proto', 'Src_Port'], axis='columns')
- 
+data = data.drop(['Proto', 'Src_Port', "Dst_Port"], axis='columns')
+data = data.fillna(0)
+
 # print the dataframe
 print(data)
 
