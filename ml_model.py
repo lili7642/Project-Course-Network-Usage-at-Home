a741fa0a721ae4f939ce88305a4d025817162a9a
Jesper Edstrom <jesper.j.edstrom@gmail.com>
2023-11-28 17:40:05 +0100

    updated code to get better accuracy. Now 65% accurasy on nn classifier

--- ml_model.py
+++ ml_model.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.neural_network import MLPClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 pd.set_option('display.max_columns', None)
 
@@ -16,9 +18,54 @@ data = pd.read_csv(f_path + csv_filename, sep="\t", na_values="NaN")
 data = data.drop(data[data.Label == "Unknown"].index)
 data = data.drop(data[data.Duration > 20000].index)
 
+#bag-worda url:sen
+vectorizer = CountVectorizer(binary=True)
+X = vectorizer.fit_transform(data['Domain_Name'])
+
+df_bow = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names_out())
+# Identify columns with only numeric names
+numeric_columns = df_bow.columns[df_bow.columns.str.isnumeric()]
+
+# Drop columns with only numeric names
+df_bow.drop(numeric_columns, axis=1, inplace=True)
+
+# print(len(data.index))
+# print(len(df_bow.index))
+data.reset_index(drop=True, inplace=True)
+df_bow.reset_index(drop=True, inplace=True)
+data = pd.concat([data, df_bow], axis=1)
+# data = data.drop('Domain_Name', axis=1)
+
+
+# Drop the large labled data point
+# data = data.drop(data[data.Label == "Netflix"].index)
+# data = data.drop(data[data.Label == "Youtube"].index)
+# data = data.drop(data[data.Label == "Facebook"].index)
+# data = data.drop(data[data.Label == "YouTube"].index)
+# data = data.drop(data[data.Label == "Gmail"].index)
+# data = data.drop(data[data.Label == "Instagram"].index)
+# data = data.drop(data[data.Label == "X"].index)
+# data = data.drop(data[data.Label == "Outlook Mail"].index)
+# data = data.drop(data[data.Label == "Steam Gaming"].index)
+# data = data.drop(data[data.Label == "Reddit"].index)
+# data = data.drop(data[data.Label == "SVT Play"].index)
+# data = data.drop(data[data.Label == "Twitch TV"].index)
+# data = data.drop(data[data.Label == "Google Drive"].index)
+# data = data.drop(data[data.Label == "Prime video"].index)
+# data = data.drop(data[data.Label == "Amazon shopping"].index)
+# data = data.drop(data[data.Label == "Google Drive"].index)
+
+
+# Normalize data
+
+columns = ['Bytes', 'Duration', 'Packets']
+for column in columns:
+    data[column] = (data[column] - data[column].min()) / (data[column].max() - data[column].min()) 
+
+
 
 # separate data into train and test data
-data = data.drop(columns=["Date", "Time", "Src_IP_Addr", "Dst_IP_Addr", "Host_IP", "Rate", "Domain_Name", "Adblocker"])
+data = data.drop(columns=["Date", "Time", "Src_IP_Addr", "Dst_IP_Addr", "Host_IP", "Rate", "Domain_Name"]) # Adblocker
 
 msk = np.random.rand(len(data)) < 0.8
 train = data[msk]
@@ -39,7 +86,10 @@ Ytest = test["Label"]
 # print(np.size(test))
 
 # # nn model using lbfgs 
-clf = MLPClassifier(solver='sgd', alpha=1e-5, hidden_layer_sizes=(10, 2), random_state=1)
+n = data["Label"].value_counts()
+print(n)
+
+clf = MLPClassifier(solver='adam',alpha=1e-5, hidden_layer_sizes=(10,10,15), random_state=1)
 
 model = clf.fit(Xtrain, Ytrain)
 
