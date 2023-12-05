27acfe675d440df9eb47d0679564f7d85d236b87
lili7642 <lili7642@student.uu.se>
2023-12-05 12:59:09 +0100

    datafix

--- split_categories.py
+++ split_categories.py
@@ -15,20 +15,20 @@ data = pd.read_csv(f_path + csv_filename, sep=",", na_values="NaN")
 # data2 = pd.read_csv(f_path + csv_hostnames, sep="\t", na_values="NaN", index_col= 0)
 
 # data = data.drop(data["192.168.8" in data["Src_IP_Addr"]].index)
-data2 = data[data["Src_IP_Addr"].str.contains("192.168.8") == False] 
+data2 = data[data["Src IP Addr"].str.contains("192.168.8") == False] 
 # data = data.drop(data[data["Src_IP_Addr"] == "192.168.8.196"].index)
 
  
 # get the dummies and store it in a variable
 dummies = pd.get_dummies(data.Proto).astype(int)
-dummies2 = pd.get_dummies(data2.Src_Port).astype(int)
+dummies2 = pd.get_dummies(data2["Src Port"]).astype(int)
  
 # Concatenate the dummies to original dataframe
 data = pd.concat([data, dummies], axis='columns')
 data = pd.concat([data, dummies2], axis='columns')
 
 # drop the values
-data = data.drop(['Proto', 'Src_Port', "Dst_Port"], axis='columns')
+data = data.drop(['Proto', 'Src Port', "Dst Port"], axis='columns')
 data = data.fillna(0)
 
 # print the dataframe
